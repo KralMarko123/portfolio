2ef11680c01341db666ef3ce668be5d2b11f4a15
KralMarko123 <kralmarko123@gmail.com>
2022-09-11 15:23:19 +0200

    changed projects to a constant

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 
-const Project = (id, videoSrc, posterSrc) => {
+const Project = ({ id, video, poster, title, description }) => {
 	return (
 		<div id={id} className="projects__project">
-			<video playsInline autoPlay muted loop poster={posterSrc}>
-				<source src={videoSrc} type="video/mp4" />
+			<video playsInline autoPlay muted loop poster={poster}>
+				<source src={video} type="video/mp4" />
 				Your browser does not support the video tag.
 			</video>
+
+			<div className="project__overlay">
+				<h1 className="project__title">{title}</h1>
+				<p className="project__description">{description}</p>
+			</div>
 		</div>
 	);
 };
---
+++ src/constants/Projects.jsx
@@ -0,0 +1,13 @@
+import cypressVideo from "../assets/videos/cypress.mp4";
+import cypressPoster from "../assets/images/cypressPoster.jpg";
+
+export const PROJECTS = [
+	{
+		id: "cypress",
+		video: cypressVideo,
+		poster: cypressPoster,
+		title: "Cypress Test Framework",
+		description:
+			"A testing framework covering numerous placeholder sites using Cypress. It consists of written E2E scenarios covered with automated scripts",
+	},
+];
