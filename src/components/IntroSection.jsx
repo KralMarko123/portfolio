a4fef255b3ba62ddd8525c84a3647da90837c6cf
MarkoMarkovikjEndava <marko.markovikj@endava.com>
2022-09-13 11:53:58 +0200

    made intro fullscreen

--- src/components/IntroSection.jsx
+++ src/components/IntroSection.jsx
@@ -7,25 +7,29 @@ const IntroSection = () => {
 	return (
 		<>
 			<section className="intro">
-				<div className="intro__text">
-					<span>Hi! I'm Marko.</span>
-					<br />
-					<p>A down-to-earth, calm, determined 23 year old software engineering student.</p>
-					<p>
-						When I'm not in front of a computer screen, I teach street dance and play pool. I am
-						currently working as a tester at{" "}
-						<a
-							href="https://careers.endava.com/en/Explore-Locations/Central-Europe/Skopje"
-							target="_blank"
-						>
-							Endava
-						</a>
-						.
-					</p>
+				<div className="intro__top">
+					<div className="intro__text">
+						<span>Hi! I'm Marko.</span>
+						<br />
+						<p>A down-to-earth, calm, determined 23 year old software engineering student.</p>
+						<p>
+							When I'm not in front of a computer screen, I teach street dance and play pool. I am
+							currently working as a tester at{" "}
+							<a
+								href="https://careers.endava.com/en/Explore-Locations/Central-Europe/Skopje"
+								target="_blank"
+							>
+								Endava
+							</a>
+							.
+						</p>
+					</div>
+					<img src={profileImage} alt="profile-image" className="intro__image" />
 				</div>
-				<img src={profileImage} alt="profile-image" className="intro__image" />
+				<div className="intro__bottom">
+					<TbArrowNarrowDown className="intro__arrow enter" />
+				</div>
 			</section>
-			<TbArrowNarrowDown className="intro__arrow enter" />
 		</>
 	);
 };
