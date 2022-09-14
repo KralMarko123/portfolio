00c0e76114b2c94d724f1ebee2993aede1e70ba3
KralMarko123 <kralmarko123@gmail.com>
2022-09-15 01:23:56 +0200

    changed bid solution due to performance issues

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -1,22 +1,9 @@
-import { React, useState, useEffect } from "react";
+import { React } from "react";
 import Project from "./Project";
 import { PROJECTS } from "../../src/constants/Projects";
 import "../styles/components/ProjectsSection.css";
 
 const ProjectsSection = () => {
-	const [isMobile, setIsMobile] = useState(false);
-
-	useEffect(() => {
-		const handleResize = () => {
-			window.innerWidth <= 767 ? setIsMobile(true) : setIsMobile(false);
-		};
-
-		window.addEventListener("resize", handleResize);
-		handleResize();
-
-		return () => window.removeEventListener("resize", handleResize);
-	}, [isMobile]);
-
 	return (
 		<section className="projects">
 			<h1 className="projects__title">Projects:</h1>
@@ -25,8 +12,8 @@ const ProjectsSection = () => {
 					<Project
 						key={p.id}
 						id={p.id}
-						video={isMobile ? p.mobileVideo : p.video}
-						poster={isMobile ? p.mobilePoster : p.poster}
+						video={window.innerWidth <= 767 ? p.mobileVideo : p.video}
+						poster={window.innerWidth <= 767 ? p.mobilePoster : p.poster}
 						title={p.title}
 						description={p.description}
 						stack={p.stack}
