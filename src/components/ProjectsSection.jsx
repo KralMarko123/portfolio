ce1ee7fa643f8bdd4c9b5623979002c2d643ccc3
KralMarko123 <kralmarko123@gmail.com>
2022-09-15 01:21:28 +0200

    changed some vids to have mobile versions

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
 import Project from "./Project";
 import { PROJECTS } from "../../src/constants/Projects";
 import "../styles/components/ProjectsSection.css";
 
 const ProjectsSection = () => {
+	const [isMobile, setIsMobile] = useState(false);
+
+	useEffect(() => {
+		const handleResize = () => {
+			window.innerWidth <= 767 ? setIsMobile(true) : setIsMobile(false);
+		};
+
+		window.addEventListener("resize", handleResize);
+		handleResize();
+
+		return () => window.removeEventListener("resize", handleResize);
+	}, [isMobile]);
+
 	return (
 		<section className="projects">
 			<h1 className="projects__title">Projects:</h1>
@@ -12,8 +25,8 @@ const ProjectsSection = () => {
 					<Project
 						key={p.id}
 						id={p.id}
-						videoPath={p.video}
-						poster={p.poster}
+						video={isMobile ? p.mobileVideo : p.video}
+						poster={isMobile ? p.mobilePoster : p.poster}
 						title={p.title}
 						description={p.description}
 						stack={p.stack}
--- src/constants/Projects.jsx
+++ src/constants/Projects.jsx
@@ -1,14 +1,25 @@
 import cypressPoster from "../assets/images/cypress.jpg";
+import cypressVideo from "../assets/videos/cypress.mp4";
 import ecinemaPoster from "../assets/images/ecinema.jpg";
+import ecinemaVideo from "../assets/videos/ecinema.mp4";
 import wbsPoster from "../assets/images/wbs.jpg";
+import wbsMobilePoster from "../assets/images/wbs_mobile.jpg";
+import wbsVideo from "../assets/videos/wbs.mp4";
+import wbsMobileVideo from "../assets/videos/wbs_mobile.mp4";
 import yePoster from "../assets/images/ye.jpg";
+import yeMobilePoster from "../assets/images/ye_mobile.jpg";
+import yeVideo from "../assets/videos/ye.mp4";
+import yeMobileVideo from "../assets/videos/ye_mobile.mp4";
 import travellandPoster from "../assets/images/travelland.jpg";
+import travellandVideo from "../assets/videos/travelland.mp4";
 
 export const PROJECTS = [
 	{
 		id: "cypress",
-		video: "cypress.mp4",
+		video: cypressVideo,
+		mobileVideo: cypressVideo,
 		poster: cypressPoster,
+		mobilePoster: cypressPoster,
 		title: "Cypress Test Framework",
 		description:
 			"A testing framework covering numerous placeholder sites using Cypress. It consists of written E2E scenarios covered with automated scripts.",
@@ -20,8 +31,10 @@ export const PROJECTS = [
 
 	{
 		id: "ecinema",
-		video: "ecinema.mp4",
+		video: ecinemaVideo,
+		mobileVideo: ecinemaVideo,
 		poster: ecinemaPoster,
+		mobilePoster: ecinemaPoster,
 		title: "ECinema App",
 		description:
 			"A project I made during my studies. Features include invoice creation, excel imports/exports, cart management, Stripe payments.",
@@ -34,8 +47,10 @@ export const PROJECTS = [
 
 	{
 		id: "nbaviewer",
-		video: "wbs.mp4",
+		video: wbsVideo,
+		mobileVideo: wbsMobileVideo,
 		poster: wbsPoster,
+		mobilePoster: wbsMobilePoster,
 		title: "WBS NBA Viewer",
 		description:
 			"A React app that sends out SPARQL queries using a third party library. It fetches NBA players from an RDF database and renders the results accordingly.",
@@ -48,8 +63,10 @@ export const PROJECTS = [
 
 	{
 		id: "yecardgame",
-		video: "ye.mp4",
+		video: yeVideo,
+		mobileVideo: yeMobileVideo,
 		poster: yePoster,
+		mobilePoster: yeMobilePoster,
 		title: "Ye Memory Game",
 		description:
 			"A simple card game featuring me and my friends. Kill some time and try to finish it before the timer runs out.",
@@ -62,8 +79,10 @@ export const PROJECTS = [
 
 	{
 		id: "travelland",
-		video: "travelland.mp4",
+		video: travellandVideo,
+		mobileVideo: travellandVideo,
 		poster: travellandPoster,
+		mobilePoster: travellandPoster,
 		title: "TravelLand Testing Framework",
 		description:
 			"A Selenium project covering the functionality of a travel agency app. The project includes DB, API & FE tests.",
