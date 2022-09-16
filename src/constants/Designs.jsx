64c17acea65aea638a1538868e4f56efcdfd7f20
KralMarko123 <kralmarko123@gmail.com>
2022-09-16 12:02:56 +0200

    added designs to design page

--- src/constants/Designs.jsx
+++ src/constants/Designs.jsx
@@ -1,6 +1,23 @@
+import advideGeneratorPoster from "../assets/images/adviceGenerator.jpg";
+import introSectionPoster from "../assets/images/introsection.jpg";
+import interactiveRatingPoster from "../assets/images/interactiveRating.jpg";
 import timeTrackingPoster from "../assets/images/timeTracking.jpg";
 import articlePoster from "../assets/images/article.jpg";
 import baseApparelPoster from "../assets/images/baseApparel.jpg";
+import coolloginPoster from "../assets/images/coollogin.jpg";
+import comingsoonPoster from "../assets/images/comingsoon.jpg";
+import fourcardPoster from "../assets/images/fourcard.jpg";
+import faqaccordionPoster from "../assets/images/faqaccordion.jpg";
+import huddlePoster from "../assets/images/huddle.jpg";
+import signupPoster from "../assets/images/introsignup.jpg";
+import nftPoster from "../assets/images/nft.jpg";
+import qrPoster from "../assets/images/qrcode.jpg";
+import profileCardPoster from "../assets/images/profilecard.jpg";
+import orderSummaryPoster from "../assets/images/ordersummary.jpg";
+import socialProofPoster from "../assets/images/socialproof.jpg";
+import statsPreviewPoster from "../assets/images/statspreview.jpg";
+import sunnysidePoster from "../assets/images/sunnyside.jpg";
+import threeColumnPoster from "../assets/images/threecolumn.jpg";
 
 const DESIGNS_BASE_URL = "https://designs.markomarkovikj.com";
 
@@ -12,14 +29,116 @@ export const DESIGNS = [
 	},
 
 	{
-		title: "Article Preview",
-		link: `${DESIGNS_BASE_URL}/articlepreview`,
-		poster: articlePoster,
+		title: "Snap Intro Section",
+		link: `${DESIGNS_BASE_URL}/introsection`,
+		poster: introSectionPoster,
+	},
+
+	{
+		title: "Advice Generator",
+		link: `${DESIGNS_BASE_URL}/advicegenerator`,
+		poster: advideGeneratorPoster,
 	},
 
 	{
 		title: "Base Apparel",
 		link: `${DESIGNS_BASE_URL}/baseapparel`,
 		poster: baseApparelPoster,
+	},
+
+	{
+		title: "Article Preview",
+		link: `${DESIGNS_BASE_URL}/articlepreview`,
+		poster: articlePoster,
+	},
+
+	{
+		title: "Interactive Rating Component",
+		link: `${DESIGNS_BASE_URL}/interactiverating`,
+		poster: interactiveRatingPoster,
+	},
+
+	{
+		title: "Cool Login Form",
+		link: `${DESIGNS_BASE_URL}/coolloginform`,
+		poster: coolloginPoster,
+	},
+
+	{
+		title: "Ping Coming Soon",
+		link: `${DESIGNS_BASE_URL}/pingcomingsoon`,
+		poster: comingsoonPoster,
+	},
+
+	{
+		title: "Four Card Feature",
+		link: `${DESIGNS_BASE_URL}/fourcardfeature`,
+		poster: fourcardPoster,
+	},
+
+	{
+		title: "Faq Accordion",
+		link: `${DESIGNS_BASE_URL}/faqaccordion`,
+		poster: faqaccordionPoster,
+	},
+
+	{
+		title: "Huddle Landing",
+		link: `${DESIGNS_BASE_URL}/huddlelanding`,
+		poster: huddlePoster,
+	},
+
+	{
+		title: "Intro Signup Form",
+		link: `${DESIGNS_BASE_URL}/introsignup`,
+		poster: signupPoster,
+	},
+
+	{
+		title: "NFT Component",
+		link: `${DESIGNS_BASE_URL}/nftpreview`,
+		poster: nftPoster,
+	},
+
+	{
+		title: "QR Code Component",
+		link: `${DESIGNS_BASE_URL}/qrcode`,
+		poster: qrPoster,
+	},
+
+	{
+		title: "Profile Card Component",
+		link: `${DESIGNS_BASE_URL}/profilecard`,
+		poster: profileCardPoster,
+	},
+
+	{
+		title: "Order Summary",
+		link: `${DESIGNS_BASE_URL}/ordersummary`,
+		poster: orderSummaryPoster,
+	},
+
+	{
+		title: "Social Proof",
+		link: `${DESIGNS_BASE_URL}/socialproof`,
+		poster: socialProofPoster,
+	},
+
+	{
+		title: "Stats Preview",
+		link: `${DESIGNS_BASE_URL}/statspreview`,
+		poster: statsPreviewPoster,
+	},
+
+	{
+		title: "Sunnyside Agency",
+		link: `${DESIGNS_BASE_URL}/sunnysideagency`,
+		poster: sunnysidePoster,
+	},
+
+	{
+		title: "Three Column Preview",
+		link: `${DESIGNS_BASE_URL}/threecolumnpreview`,
+		poster: threeColumnPoster,
 	},
 ];
