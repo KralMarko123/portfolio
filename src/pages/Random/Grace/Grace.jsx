538212a15d9c147e8b337d0cfee2e792f8bef47f
KralMarko123 <kralmarko123@gmail.com>
2023-11-02 22:57:35 +0100

    added confetti

--- src/pages/Random/Grace/Grace.jsx
+++ src/pages/Random/Grace/Grace.jsx
@@ -1,26 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import dancingSkeleton from "../../../assets/gifs/dancing skeleton.gif";
-import confetti from "https://esm.run/canvas-confetti@1";
 import { useNavigate } from "react-router-dom";
 import { ROUTES } from "../../../constants/ROUTES";
+import ConfettiExplosion from "react-confetti-explosion";
 import "./Grace.css";
 
 const Grace = () => {
+	const [isExploding, setIsExploding] = useState(false);
+	const navigate = useNavigate();
 	const githubCommit =
 		"https://github.com/KralMarko123/portfolio/commit/815677405d2bb8e90aa1bbfb78e7df8c6396e8cc";
-	const navigate = useNavigate();
+
 	return (
 		<div className="page grace">
+			<div className="grace-confetti">
+				{isExploding && (
+					<ConfettiExplosion particleCount={200} onComplete={() => setIsExploding(false)} />
+				)}
+			</div>
 			<img
 				className="grace-skeleton"
 				src={dancingSkeleton}
 				alt="dancing-skeleton"
-				onClick={() =>
-					confetti({
-						particleCount: 200,
-						spread: 100,
-					})
-				}
+				onClick={() => setIsExploding(true)}
 			/>
 
 			<p className="grace-description">
