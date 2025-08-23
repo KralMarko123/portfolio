7a03e5c52f2ebb791ed5a1b8a0ff9b49617ecd70
KralMarko123 <kralmarko123@gmail.com>
2025-08-23 19:38:37 +0200

    feat: added CV to site

--- src/constants/LINKS.jsx
+++ src/constants/LINKS.jsx
@@ -1,18 +1,25 @@
-import { FaMailBulk, FaGithub, FaLinkedin } from 'react-icons/fa';
+import { FaMailBulk, FaGithub, FaLinkedin } from "react-icons/fa";
+import { TbFileCv } from "react-icons/tb";
+import markoCV from "../assets/pdfs/Marko Markovikj CV.pdf";
 
 export const CONTACT_LINKS = [
-	{
-		link: 'mailto:kralmarko123@gmail.com',
-		icon: <FaMailBulk />
-	},
+  {
+    link: "mailto:kralmarko123@gmail.com",
+    icon: <FaMailBulk />,
+  },
 
-	{
-		link: 'https://github.com/KralMarko123',
-		icon: <FaGithub />
-	},
+  {
+    link: "https://github.com/KralMarko123",
+    icon: <FaGithub />,
+  },
 
-	{
-		link: 'https://www.linkedin.com/in/marko-markovikj/',
-		icon: <FaLinkedin />
-	}
+  {
+    link: "https://www.linkedin.com/in/marko-markovikj/",
+    icon: <FaLinkedin />,
+  },
+
+  {
+    link: markoCV,
+    icon: <TbFileCv />,
+  },
 ];
